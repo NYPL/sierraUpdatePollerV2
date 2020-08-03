0e8f9da40d9a291b7e477e4bc072aa2506cd80b7
Mike Benowitz <mwbenowitz@gmail.com>
2020-08-03 18:24:55 -0400

    SCC-2189 Do not base64 encode avro records
    By default the ruby utils avro client base64 encodes avro records, which is the desired behavior. But Kinesis also encodes these records, which poses problems further down the pipeline. This sets the behavior to omit this encoding and a test to ensure that this remains set

--- spec/sierra_record_spec.rb
+++ spec/sierra_record_spec.rb
@@ -3,14 +3,14 @@ require_relative './spec_helper'
 
 describe SierraBatch::SierraRecord do
     before(:each) {
-        mock_record = mock()
-        @test_record = SierraBatch::SierraRecord.new mock_record
+        @mock_record = mock()
+        @test_record = SierraBatch::SierraRecord.new @mock_record
     }
 
     describe '#encode' do
         it 'should set an encoded record from avro' do
             $avro_client = mock()
-            $avro_client.stubs(:encode).returns('avro_record')
+            $avro_client.stubs(:encode).once.with(@mock_record, base64=false).returns('avro_record')
             
             @test_record.encode
             expect(@test_record.encoded_record).to eq('avro_record')
